3a3dcc4782fd5a92dd81a0714bca5940f1e5a688
Retro <70205403+retrouser955@users.noreply.github.com>
2025-07-03 22:36:50 +0800

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import * as d3 from "d3";
+  import { onMount } from "svelte";
+
+  let svgEle: SVGElement;
+
+  onMount(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+  });
+</script>
+
+<div class="w-screen h-screen">
+  <svg bind:this={svgEle}></svg>
+</div>
